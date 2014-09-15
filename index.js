8156f7376e38e2110c72a0e09c862705b9978311
fengmk2 <fengmk2@gmail.com>
2014-09-15 21:52:04 +0800

    Generate Content-Disposition header appropriate for the filename.
    Come from https://github.com/strongloop/express/blob/master/lib/utils.js#L114

--- index.js
+++ index.js
@@ -5,7 +5,34 @@
  */
 
 /**
+ * Module dependencies.
+ */
+
+var basename = require('path').basename
+
+/**
  * Module exports.
  */
 
-module.exports = {};
+/**
+ * Generate Content-Disposition header appropriate for the filename.
+ * non-ascii filenames are urlencoded and a filename* parameter is added
+ *
+ * @param {String} filename
+ * @return {String}
+ * @api public
+ * @see http://blog.fastmail.fm/2011/06/24/download-non-english-filenames/
+ */
+
+exports.contentDisposition = function (filename) {
+  var ret = 'attachment'
+  if (filename) {
+    filename = basename(filename);
+    // if filename contains non-ascii characters, add a utf-8 version ala RFC 5987
+    ret = /[^\040-\176]/.test(filename)
+      ? 'attachment; filename="' + encodeURI(filename) + '"; filename*=UTF-8\'\'' + encodeURI(filename)
+      : 'attachment; filename="' + filename + '"'
+  }
+
+  return ret
+};
